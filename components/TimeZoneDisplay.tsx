2592c339a0df81eca53f04594e61b2a1a00eeea0
Chones17 <chones17@byui.edu>
2023-09-08 00:01:23 -0600

    Update TimeZoneDisplay.tsx
    Refactor time to automatically update

--- components/TimeZoneDisplay.tsx
+++ components/TimeZoneDisplay.tsx
@@ -1,17 +1,14 @@
 'use client'
 import React, { ChangeEvent, useState, useEffect } from 'react'
 
-function TimeTest(timeZone: string): string {
-    const date: Date = new Date()
-    const time: string = new Intl.DateTimeFormat('en-US', {timeStyle: 'medium', timeZone: timeZone}).format(date)
-    return (time)
-}
-
 export default function TimeZoneDisplay(): JSX.Element {
     
     // Set the states for time zone and New Year countdown
-    const [selectedTimeZone, setSelectedTimeZone] = useState<string>('')
+    const [selectedTimeZone, setSelectedTimeZone] = useState<string>('America/Denver')
     const [newYearCountdown, setNewYearCountdown] = useState<string>('')
+    const [firstTimeZone, setFirstTimeZone] = useState<string>('')
+    const [secondTimeZone, setSecondTimeZone] = useState<string>('')
+    const [thirdTimeZone, setThirdTimeZone] = useState<string>('')
 
     const timeZones: string[] = [
         'America/Denver',
@@ -23,47 +20,49 @@ export default function TimeZoneDisplay(): JSX.Element {
 
     const timeZoneChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setSelectedTimeZone(event.target.value)
-    };
+    }
 
     useEffect(() => {
-        const updateCountdown = () => {
-            if (selectedTimeZone) {
-                const newYearDate = new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0, 0)
-                const currentDate = new Date()
-                
-                // Calculate time until New Year's
-                const timeDifference = newYearDate.getTime() - currentDate.getTime();
-                const daysUntilNewYear = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-                const hoursUntilNewYear = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                const minutesUntilNewYear = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-                const secondsUntilNewYear = Math.floor((timeDifference % (1000 * 60)) / 1000);
-        
-                const countdownString = `${daysUntilNewYear} days ${hoursUntilNewYear} hours ${minutesUntilNewYear} minutes ${secondsUntilNewYear} seconds`;
-        
-                setNewYearCountdown(countdownString);
-            } else {
-                setNewYearCountdown('');
-            }
+        const updateTime = () => {
+            const newYearDate = new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0, 0)
+            const currentDate = new Date()
+            const firstTime: string = new Intl.DateTimeFormat('en-US', {timeStyle: 'medium', timeZone: 'America/Denver'}).format(currentDate)
+            const secondTime: string = new Intl.DateTimeFormat('en-US', {timeStyle: 'medium', timeZone: 'Asia/Kolkata'}).format(currentDate)
+            const thirdTime: string = new Intl.DateTimeFormat('en-US', {timeStyle: 'medium', timeZone: selectedTimeZone}).format(currentDate)
+            
+            // Calculate time until New Year's
+            const timeDifference = newYearDate.getTime() - currentDate.getTime()
+            const daysUntilNewYear = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+            const hoursUntilNewYear = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+            const minutesUntilNewYear = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+            const secondsUntilNewYear = Math.floor((timeDifference % (1000 * 60)) / 1000)
+    
+            const countdownString = `${daysUntilNewYear} days ${hoursUntilNewYear} hours ${minutesUntilNewYear} minutes ${secondsUntilNewYear} seconds`
+    
+            setNewYearCountdown(countdownString)
+            setFirstTimeZone(firstTime)
+            setSecondTimeZone(secondTime)
+            setThirdTimeZone(thirdTime)
         };
 
-        const interval = setInterval(updateCountdown, 1000);
+        const interval = setInterval(updateTime, 1000)
 
-        return () => clearInterval(interval);
+        return () => clearInterval(interval)
     }, [selectedTimeZone]);
 
     return (
         <div>
-            <h2>{timeZones[0]}</h2>
-            <p>{TimeTest(timeZones[0])}</p>
-            <h2>{timeZones[1]}</h2>
-            <p>{TimeTest(timeZones[1])}</p>
+            <h2 className="font-bold">{timeZones[0]}</h2>
+            <p className="mb-5">{firstTimeZone}</p>
+            <h2 className="font-bold">{timeZones[1]}</h2>
+            <p className="mb-5">{secondTimeZone}</p>
             <select value={selectedTimeZone} onChange={timeZoneChange}>
-                <option value="">--</option>
                 {timeZones.map((zone) => (
                     <option key={zone} value={zone}>{zone}</option>
                ))}
             </select>
-            <h2>New Year's Countdown</h2>
+            <p className="mb-5">{thirdTimeZone}</p>
+            <h2 className="font-bold">New Year's Countdown</h2>
             <p>{newYearCountdown}</p>
         </div>
     );
